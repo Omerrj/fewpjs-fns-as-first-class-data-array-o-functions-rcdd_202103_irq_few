300feae046f009b9c635889897f9465ef9eb093d
Omer Sabah <omerrj99@gmail.com>
2021-04-23 22:40:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,21 +9,34 @@ function leashDog(dogName, dogBreed) {
   return text
 }
 function walkToPark(dogName, dogBreed) {
+<<<<<<< HEAD
   const text=`Walk to the park with ${dogName} the ${dogBreed}`
+=======
+  const text=`Wlak to the park with ${dogName} the ${dogBreed}`
+>>>>>>> e57f5fdb3b50a53db6390b1ba03eae3a76585748
   console.log(text);
   return text
 }
 function throwFrisbee(dogName, dogBreed) {
+<<<<<<< HEAD
   const text=`Throw the frisbee for ${dogName} the ${dogBreed}`
+=======
+  const text=`Wake ${dogName} the ${dogBreed}`
+>>>>>>> e57f5fdb3b50a53db6390b1ba03eae3a76585748
   console.log(text);
   return text
 }
 function walkHome(dogName, dogBreed) {
+<<<<<<< HEAD
   const text=`Walk home with ${dogName} the ${dogBreed}`
+=======
+  const text=`Wake ${dogName} the ${dogBreed}`
+>>>>>>> e57f5fdb3b50a53db6390b1ba03eae3a76585748
   console.log(text);
   return text
 }
 function unleashDog(dogName, dogBreed) {
+<<<<<<< HEAD
   const text=`Unleash ${dogName} the ${dogBreed}`
   console.log(text);
   return text
@@ -40,3 +53,9 @@ function exerciseDog(dogName,dogBreed){
   return array;
 }
 exerciseDog('dog','breed');
+=======
+  const text=`Wake ${dogName} the ${dogBreed}`
+  console.log(text);
+  return text
+}
+>>>>>>> e57f5fdb3b50a53db6390b1ba03eae3a76585748
